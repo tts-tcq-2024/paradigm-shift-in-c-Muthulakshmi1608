f92cdeb1029cf111f5c7728196aa8d907c8567a7
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-06-30 18:53:06 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -6,13 +6,14 @@ bool range(float value, float lower_threshhold,float upper_threshhold){
  return((value < lower_threshhold) && (value > upper_threshhold));
 }
 
-bool chargeRange(float value, float upper_threshhold){
- return(value > upper_threshhold);
+bool chargeRange(float value, float threshhold){
+ return(value > threshhold);
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
  bool a;
- a=(range(temperature,0,45) || (range(soc,20,80)) || (chargeRange(chargeRate,upper_threshhold)));
+ float threshhold;
+ a=(range(temperature,0,45) || (range(soc,20,80)) || (chargeRange(chargeRate,threshhold)));
  return a;
 }
 
