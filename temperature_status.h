40744b5419845d019aaf92705e3d87faa212e7ca
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-28 20:14:50 +0530

    Create temperature_status.h

---
+++ temperature_status.h
@@ -0,0 +1,16 @@
+#ifndef TEMPERATURE_STATUS_H
+#define TEMPERATURE_STATUS_H
+
+typedef struct {
+    int lower_bound;
+    int upper_bound;
+    int (*temperature_ptr)();
+} TEMP_RANGE;
+
+int get_soc_status(float soc);
+int low_temp_breach();
+int low_temp_warning();
+int normal();
+int high_temp_warning();
+int high_temp_breach();
+#endif
