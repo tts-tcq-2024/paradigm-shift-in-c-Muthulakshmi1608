3f9e5da1d26e8c202448db127fd2aeb0c9b42af5
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-28 17:37:24 +0530

    Update soc_checker.c

--- soc_checker.c
+++ soc_checker.c
@@ -34,9 +34,9 @@ int high_soc_breach()
   return out_of_range_flg;
 }
 
-int soc_range_check(SOC_RANGE soc_lower_limit_check,SOC_RANGE soc_lower_limit_check, int soc)
+int soc_range_check(SOC_RANGE *soc_limit_check, int soc)
 {
-  if (soc >= soc_ranges[i].lower_bound && soc <= soc_ranges[i].upper_bound) {
+  if ((soc >= soc_limit_check->lower_bound) && (soc <= soc_limit_check->upper_bound)) {
     return 1;
   }
   return 0;
@@ -45,8 +45,7 @@ int soc_range_check(SOC_RANGE soc_lower_limit_check,SOC_RANGE soc_lower_limit_ch
 int get_soc_status(int soc) {
   
     for (int i = 0; i < sizeof(soc_ranges) / sizeof(soc_ranges[0]); i++) {
-       // if (soc >= soc_ranges[i].lower_bound && soc <= soc_ranges[i].upper_bound) {
-       if(soc_range_check(soc_ranges[i].lower_bound,soc_ranges[i].upper_bound,soc)){
+       if(soc_range_check(&soc_ranges[i],soc)){
             out_of_range_flg = soc_ranges[i].soc_ptr;
             return out_of_range_flg;
         }
