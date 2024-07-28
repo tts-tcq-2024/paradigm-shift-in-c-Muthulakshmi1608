47ad4515facf9365573f2369b92124454c94b518
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-28 17:57:12 +0530

    Update soc_checker.h

--- soc_checker.h
+++ soc_checker.h
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <assert.h>
-#include <stdbool.h>
+
+#ifndef SOC_CHECKER_H
+#define SOC_CHECKER_H
 
 typedef struct {
     int lower_bound;
@@ -8,7 +8,13 @@ typedef struct {
     int *soc_ptr;
 } SOC_RANGE;
 
-SOC_RANGE soc_ranges[];
+SOC_RANGE soc_ranges[] = {
+    { 0, 20, "low_soc_breach" },
+    { 21, 24, "low_soc_warning" },
+    { 25, 75, "normal" },
+    { 76, 80, "high_soc_warning" },
+    { 81, 100, "high_soc_breach" }
+};
 
 int get_soc_status(int soc);
 int low_soc_breach();
@@ -17,3 +23,5 @@ int normal();
 int high_soc_warning();
 int high_soc_breach();
 int soc_range_check(SOC_RANGE *soc_limit_check, int soc);
+
+#endif
