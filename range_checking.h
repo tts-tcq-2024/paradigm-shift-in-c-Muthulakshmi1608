84de3b5c987bc417e82561c03dc5e7037d9f4705
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-28 21:09:51 +0530

    Create range_checking.h

---
+++ range_checking.h
@@ -0,0 +1,13 @@
+#ifndef RANGE_CHECKING_H
+#define RANGE_CHECKING_H
+typedef struct {
+    int lower_bound;
+    int upper_bound;
+    int (*battery_status_ptr)();
+} BATTERY_STATUS_RANGE;
+
+int get_battery_status(float value);
+int range_check(BATTERY_STATUS_RANGE *limit_check, float value);
+
+extern BATTERY_STATUS_RANGE soc_ranges[10];
+#endif
