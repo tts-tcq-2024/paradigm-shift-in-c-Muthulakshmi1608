ccec9608cbca93ee7435413c3d3b25460ba34c64
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-28 21:24:48 +0530

    Update range_checking.c

--- range_checking.c
+++ range_checking.c
@@ -1,6 +1,10 @@
 #include "soc_status.h"
 #include "temperature_status.h"
 #include "range_checking.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+bool out_of_range_flg=true;
 
 int range_check(BATTERY_STATUS_RANGE *limit_check, float value)
 {
@@ -13,7 +17,7 @@ int range_check(BATTERY_STATUS_RANGE *limit_check, float value)
 int get_battery_status(float value) {
     for (int i = 0; i < sizeof(ranges) / sizeof(ranges[0]); i++) {
        if(range_check(&ranges[i],value)){
-            out_of_range_flg = ranges[i].soc_ptr();
+            out_of_range_flg = ranges[i].battery_status_ptr();
             return out_of_range_flg;
         }
     }
