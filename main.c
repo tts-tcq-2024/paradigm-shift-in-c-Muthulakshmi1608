89667a55884c12677240599cc2d10568f45d87d6
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-28 18:02:36 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+#include "soc_checker.h"
+int batteryIsOk(float temperature, float soc, float chargeRate)
+{
+  return (get_soc_status(soc));
+}
+
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
