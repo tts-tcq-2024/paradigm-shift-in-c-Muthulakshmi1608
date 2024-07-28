01a4d49b7406a816440d3df0594cec73ad9225e9
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-28 17:43:52 +0530

    Update battery_checker_initialization.c

--- battery_checker_initialization.c
+++ battery_checker_initialization.c
@@ -1,4 +1,7 @@
+#ifndef BATTERY_CHECKER_INITIALIZATION_H
+#define BATTERY_CHECKER_INITIALIZATION_H
 
+#include "soc_checker.h"
 SOC_RANGE soc_ranges[] = {
     { 0, 20, "low_soc_breach" },
     { 21, 24, "low_soc_warning" },
@@ -6,3 +9,5 @@ SOC_RANGE soc_ranges[] = {
     { 76, 80, "high_soc_warning" },
     { 81, 100, "high_soc_breach" }
 };
+
+#endif
