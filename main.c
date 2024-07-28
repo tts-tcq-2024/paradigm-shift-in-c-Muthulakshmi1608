e444794ccbb375ecbee8a3be43cd5513fb103f58
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-28 19:55:56 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
 #include <assert.h>
 #include "soc_checker.h"
 
-SOC_RANGE soc_ranges[] = {
-    { 0.0, 20.0, low_soc_breach},
-    { 21.0, 24.0, low_soc_warning},
-    { 25.0, 75.0, normal},
-    { 76.0, 80.0, high_soc_warning},
-    { 81.0, 100.0, high_soc_breach}
-};
+#define WARNING_SOC_CHECK TRUE
 int batteryIsOk(float temperature, float soc, float chargeRate)
 {
   return (get_soc_status(soc));
