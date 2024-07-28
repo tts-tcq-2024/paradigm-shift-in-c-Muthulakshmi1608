a9776eefba6e497cba8fa451ed1aa83b82c42498
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-07-28 18:16:49 +0530

    Update soc_checker.h

--- soc_checker.h
+++ soc_checker.h
@@ -18,11 +18,11 @@ int batteryIsOk(float temperature, float soc, float chargeRate);
 int soc_range_check(SOC_RANGE *soc_limit_check, float soc);
 
 SOC_RANGE soc_ranges[] = {
-    { 0.0, 20.0, low_soc_breach()},
-    { 21.0, 24.0, low_soc_warning()},
-    { 25.0, 75.0, normal()},
-    { 76.0, 80.0, high_soc_warning()},
-    { 81.0, 100.0, high_soc_breach()}
+    { 0.0, 20.0, low_soc_breach},
+    { 21.0, 24.0, low_soc_warning},
+    { 25.0, 75.0, normal},
+    { 76.0, 80.0, high_soc_warning},
+    { 81.0, 100.0, high_soc_breach}
 };
 
 #endif
