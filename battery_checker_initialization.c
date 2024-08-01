2a33b0cafc91e99992e675eb25791af184f8cbfb
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-08-01 21:59:55 +0530

    Update battery_checker_initialization.c

--- battery_checker_initialization.c
+++ battery_checker_initialization.c
@@ -8,7 +8,7 @@ BATTERY_STATUS_RANGE ranges[] = {
     { 0.0, 20.0, low_soc_breach}, { 21.0, 24.0, low_soc_warning},{ 25.0, 75.0, normal},{ 76.0, 80.0, high_soc_warning},{ 81.0, 100.0, high_soc_breach}
 };
 #else
-BATTERY_STATUS_RANGE soc_ranges[] = {
+BATTERY_STATUS_RANGE ranges[] = {
     { 0.0, 20.0, low_soc_breach},{ 25.0, 75.0, normal},{ 81.0, 100.0, high_soc_breach}
 };
 #endif
