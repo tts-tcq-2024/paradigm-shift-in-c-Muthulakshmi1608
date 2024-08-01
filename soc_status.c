ca403606a6f77bad36eb476e4251d3a56664d070
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-08-01 21:24:37 +0530

    Update soc_status.c

--- soc_status.c
+++ soc_status.c
@@ -2,38 +2,37 @@
 #include <stdbool.h>
 #include "soc_status.h"
 
-bool out_of_range_flg = true;
 
 int low_soc_breach()
 {
   printf("State of Charge out of range!\n");
   out_of_range_flg = false;
-  return out_of_range_flg;
+  return 1;
 }
 
 int low_soc_warning()
 {
   printf("Warning: Approaching discharge\n");
-  return out_of_range_flg;
+  return 1;
 }
 
 int normal()
 {
   printf("SOC is normal");
-  return out_of_range_flg;
+  return 1;
 }
 
 int high_soc_warning()
 {
   printf("Warning: Approaching charge-peak\n");
-  return out_of_range_flg;
+  return 1;
 }
 
 int high_soc_breach()
 {
   printf("State of Charge out of range!\n");
   out_of_range_flg = false;
-  return out_of_range_flg;
+  return 0;
 }
 
 
