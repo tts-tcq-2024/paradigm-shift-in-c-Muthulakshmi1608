71cca6bafbd0b741e45ea5f37879cba269b5b6e7
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-08-01 22:39:02 +0530

    Update main.c

--- main.c
+++ main.c
@@ -10,8 +10,9 @@ BATTERY_STATUS_RANGE soc_ranges[] = {
     { 0.0, 20.0, low_soc_breach}, { 21.0, 24.0, low_soc_warning},{ 25.0, 75.0, normal},{ 76.0, 80.0, high_soc_warning},{ 81.0, 100.0, high_soc_breach}
 #else
     { 0.0, 20.0, low_soc_breach},{ 25.0, 75.0, normal},{ 81.0, 100.0, high_soc_breach}
-};
 #endif
+};
+
 
 
 BATTERY_STATUS_RANGE temp_ranges[] = {
@@ -20,8 +21,9 @@ BATTERY_STATUS_RANGE temp_ranges[] = {
 #else
 BATTERY_STATUS_RANGE temp_ranges[] = {
     { -20.0, 0.0, low_temp_breach},{ 2.25, 42.75, temp_normal},{ 45.0, 100.0, high_temp_breach}
-};
 #endif
+};
+
 
 int batteryIsOk(float temperature, float soc, float chargeRate)
 {
