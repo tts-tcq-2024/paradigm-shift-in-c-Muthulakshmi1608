97f5c5ce79bf8318499ca536c7e1137c89985070
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-08-01 21:25:26 +0530

    Update temperature_status.c

--- temperature_status.c
+++ temperature_status.c
@@ -1,35 +1,32 @@
 #include "temperature_status.h"
 
-bool out_of_range_temp_flg = true;
 
 int low_temp_breach()
 {
   printf("Temperature out of range!\n");
-  out_of_range_temp_flg = false;
-  return out_of_range_temp_flg;
+  return 1;
 }
 
 int low_temp_warning()
 {
   printf("Warning: Approaching discharge\n");
-  return out_of_range_temp_flg;
+  return 1;
 }
 
 int temp_normal()
 {
   printf("Temperature is normal");
-  return out_of_range_temp_flg;
+  return 1;
 }
 
 int high_temp_warning()
 {
   printf("Warning: Approaching charge-peak\n");
-  return out_of_range_temp_flg;
+  return 1;
 }
 
 int high_temp_breach()
 {
   printf("Temperature out of range!\n");
-  out_of_range_temp_flg = false;
-  return out_of_range_temp_flg;
+  return 0;
 }
