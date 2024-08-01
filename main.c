dc21b235372b2382d8539cfb34337155b94dea45
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-08-01 22:03:12 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 
 #define WARNING_SOC_CHECK TRUE
 #define WARNING_TEMP_CHECK TRUE
+#define SOC_CHECK TRUE
+#define TEMP_CHECK TRUE
 int batteryIsOk(float temperature, float soc, float chargeRate)
 {
   return (get_battery_status(soc) && get_battery_status(temperature));
