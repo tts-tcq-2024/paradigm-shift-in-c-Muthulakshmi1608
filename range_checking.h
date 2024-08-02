f91a08dc54ccfec7682e9b4d962139bc8e791303
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-08-02 18:42:00 +0530

    Update range_checking.h

--- range_checking.h
+++ range_checking.h
@@ -6,16 +6,9 @@ typedef struct {
     int (*battery_status_ptr)();
 } BATTERY_STATUS_RANGE;
 
-//typedef struct {
-  //  int lower_bound;
-   // int upper_bound;
-    //int (*battery_status_chargerate_ptr)();
-//} BATTERY_STATUS_RANGE_CHARGERATE;
 
 int get_battery_status(float value,BATTERY_STATUS_RANGE ranges[], int size);
-//int get_battery_chargerate_status(float value,BATTERY_STATUS_RANGE_CHARGERATE ranges[], int size);
 int range_check(BATTERY_STATUS_RANGE *limit_check, float value);
-//int chargerate_range_check(BATTERY_STATUS_RANGE_CHARGERATE *limit_check, float value);
 
 extern BATTERY_STATUS_RANGE soc_ranges[10];
 extern BATTERY_STATUS_RANGE temp_ranges[10];
