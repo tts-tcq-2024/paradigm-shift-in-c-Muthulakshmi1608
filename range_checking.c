96d65281b3fde44a670d6b976407abdd3421c8b5
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-08-02 18:41:56 +0530

    Update range_checking.c

--- range_checking.c
+++ range_checking.c
@@ -13,13 +13,6 @@ int range_check(BATTERY_STATUS_RANGE *limit_check, float value)
   return 0;
 }
 
-//int chargerate_range_check(BATTERY_STATUS_RANGE_CHARGERATE *limit_check, float value)
-//{
- // if ((value >= limit_check->lower_bound)&&(value <= limit_check->upper_bound)) {
-  //  return 1;
-  //}
-  //return 0;
-//}
 
 int get_battery_status(float value,BATTERY_STATUS_RANGE ranges[], int size) {
     for (int i = 0; i < size; i++) {
@@ -28,12 +21,5 @@ int get_battery_status(float value,BATTERY_STATUS_RANGE ranges[], int size) {
         }
     }
 }
-//int get_battery_chargerate_status(float value,BATTERY_STATUS_RANGE_CHARGERATE ranges[], int size)
-//{
-  //for (int i = 0; i < size; i++) {
-    //    if (chargerate_range_check(&ranges[i], value)) {
-      //      return ranges[i].battery_status_chargerate_ptr();
-       // }
-    //}
-//}
+
   
