b0bbe170f319710f94fb885cd1fd89b058a76f66
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-08-24 18:41:00 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,41 +1,42 @@
 #include <assert.h>
-#include "soc_status.h"
-#include "temperature_status.h"
 #include "range_checking.h"
-#include "chargerate_status.h"
 
-#define WARNING_SOC_CHECK TRUE
-#define WARNING_TEMP_CHECK TRUE
-#define WARNING_CHARGERATE_CHECK TRUE
-BATTERY_STATUS_RANGE soc_ranges[] = {
-#ifdef WARNING_SOC_CHECK
-    { 0.0, 20.0, "low_soc_breach",0}, { 21.0, 24.0, "low_soc_warning",1},{ 25.0, 75.0, "normal",1},{ 76.0, 80.0, high_soc_warning},{ 81.0, 100.0, high_soc_breach}
-#else
-    { 0.0, 20.0, low_soc_breach},{ 21.0, 80.0, normal},{ 81.0, 100.0, high_soc_breach}
-#endif
-};
-
-
-
-BATTERY_STATUS_RANGE temp_ranges[] = {
-#ifdef WARNING_TEMP_CHECK
-    { -20.0, 0.0, low_temp_breach}, { 0.0, 2.25, low_temp_warning},{ 2.25, 42.75, temp_normal},{ 42.76, 45.0, high_temp_warning},{ 46.0, 100.0, high_temp_breach}
-#else
-    { -20.0, 0.0, low_temp_breach},{ 0.1, 45.0, temp_normal},{ 46.0, 100.0, high_temp_breach}
-#endif
-};
-
-BATTERY_STATUS_RANGE chargerate_ranges[] = {
-#ifdef WARNING_CHARGERATE_CHECK
-    { 0.00, 0.75, chargerate_normal},{ 0.76, 0.80, high_chargerate_warning},{ 0.90, 2.00, high_chargerate_breach}
-#else
-    { 0.00, 0.80, chargerate_normal},{ 0.90, 2.00, high_chargerate_breach}
-#endif
-};
-
-
+void add_soc_parameter(BatteryParameter *bp) {
+    strcpy(bp->name, "soc");  // Properly copying the string to the char array
+    
+    bp->ranges[0] = (Battery_Status_Range){0.0, 20.0, "State of Charge out of range!", 0};
+    bp->ranges[1] = (Battery_Status_Range){21.0, 24.0, "Warning: Approaching discharge", 1};
+    bp->ranges[2] = (Battery_Status_Range){24.0, 75.0, "SOC is normal", 3};
+    bp->ranges[3] = (Battery_Status_Range){76.0, 80.0, "Warning: Approaching charge-peak", 1};
+    bp->ranges[4] = (Battery_Status_Range){81.0, 100.0, "State of Charge out of range!", 0};
+    
+    bp->isEarlyWarningRequired = false;  // Set early warning flag to false for testing
+}
+void add_temp_parameter(BatteryParameter *temp_bp) {
+    strcpy(temp_bp->name, "temperature");  // Properly copying the string to the char array
+    
+    temp_bp->ranges[0] = (Battery_Status_Range){-20.0, 0.0, "Temperature out of range!", 0};
+    temp_bp->ranges[1] = (Battery_Status_Range){0.0, 2.25, "Warning: Approaching discharge", 1};
+    temp_bp->ranges[2] = (Battery_Status_Range){2.25, 42.75, "Temperature is normal", 3};
+    temp_bp->ranges[3] = (Battery_Status_Range){42.76, 45, "Warning: Approaching charge-peak", 1};
+    temp_bp->ranges[4] = (Battery_Status_Range){46, 100.0, "Temperature out of range!", 0};
+    
+    temp_bp->isEarlyWarningRequired = false;  // Set early warning flag to false for testing
+}
+void add_charge_parameter(BatteryParameter *charge_bp) {
+    strcpy(charge_bp->name, "chargerate");  // Properly copying the string to the char array
+    
+    charge_bp->ranges[0] = (Battery_Status_Range){0, 0.75, "chargerate normal!", 0};
+    charge_bp->ranges[1] = (Battery_Status_Range){0.76,0.80, "Warning: Approaching charge-peak", 1};
+    charge_bp->ranges[2] = (Battery_Status_Range){0.90, 2.00, "chargerate is out of range!", 3};
+    
+    charge_bp->isEarlyWarningRequired = false;  // Set early warning flag to false for testing
+}
 int batteryIsOk(float temperature, float soc, float chargeRate)
 {
+ add_soc_parameter(&bp);
+ add_temp_parameter(&temp_bp);
+ add_charge_parameter(&charge_bp);
   int soc_status = get_battery_status(soc, soc_ranges, sizeof(soc_ranges) / sizeof(soc_ranges[0]));
   int temp_status = get_battery_status(temperature, temp_ranges, sizeof(temp_ranges) / sizeof(temp_ranges[0]));
   int chargerate_status = get_battery_status(chargeRate, chargerate_ranges, sizeof(chargerate_ranges) / sizeof(chargerate_ranges[0]));
@@ -47,5 +48,4 @@ int main() {
   assert(!batteryIsOk(50, 85, 0.00)==true);// SOC and temp outofrange,chargerate normal
   assert(batteryIsOk(1, 77, 0.77));//Warning
    assert(!batteryIsOk(10, 50, 0.90));// chargerate outofrange,SOC and temp normal
-  
 }
