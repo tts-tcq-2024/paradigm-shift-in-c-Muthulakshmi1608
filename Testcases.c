b0772d747ace4ac552defc8c8d6f0f6e6809f12e
Muthulakshmi1608 <muthulakshmisanthakumar@gmail.com>
2024-08-25 21:31:26 +0530

    Update Testcases.c

--- Testcases.c
+++ Testcases.c
@@ -6,14 +6,14 @@ void testchargerateNormal() {
     assert(!batteryIsOk(50, 85, 0.00)==true);
 }
 
-void testbatterywarning() {
-    assert(batteryIsOk(2, 78, 0.78));
-}
-
 void testtemperaturewarning() {
    assert(!batteryIsOk(10, 50, 0.9));
 }
 
 void testBatteryNormal() {
     assert(batteryIsOk(25, 70, 0.70));
+}
+
+void testbatterywarning() {
+    assert(batteryIsOk(2, 78, 0.78));
 }
